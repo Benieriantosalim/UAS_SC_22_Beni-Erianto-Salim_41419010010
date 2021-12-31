aaa8491b6e9fef2e07c2bfefbff5787cda7faa61
Benieriantosalim <96732478+Benieriantosalim@users.noreply.github.com>
2021-12-31 17:16:27 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -25,7 +25,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/AchmadBurhan10/UAS_SC_18_Achmad-Burhanudin_41419010005/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/Benieriantosalim/UAS_SC_22_Beni-Erianto-Salim_41419010010/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
